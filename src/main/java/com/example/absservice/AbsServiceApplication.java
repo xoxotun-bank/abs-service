e44ae02304c9a4ff098f4974b3127c39cdf40014
Enorezero <den.startsev.2016@mail.ru>
2025-03-06 18:36:08 +1000

    init

---
+++ src/main/java/com/example/absservice/AbsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.absservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/absservice/AbsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.absservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
